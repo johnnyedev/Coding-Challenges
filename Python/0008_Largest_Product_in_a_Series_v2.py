db799a20bf30ab7e5f973d57ca26f82577ef293a
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-02-21 00:00:25 -0600

    Update and rename 8_Largest_Product_in_a_Series_v2.py to 0008_Largest_Product_in_a_Series_v2.py

--- Python/0008_Largest_Product_in_a_Series_v2.py
+++ Python/0008_Largest_Product_in_a_Series_v2.py
@@ -1,8 +1,9 @@
 # Project Euler
-# Problem 8: https://projecteuler.net/problem=8
+# Problem 8: Largest Product in a Series
+# https://projecteuler.net/problem=8
 
-# "The four adjacent digits in the 1000-digit number that have the greatest product are 9 x 9 x 8 x 9 = 5832.
-
+# The four adjacent digits in the 1000-digit number that have the greatest product are 9 x 9 x 8 x 9 = 5832.
+#
 # 73167176531330624919225119674426574742355349194934
 # 96983520312774506326239578318016984801869478851843
 # 85861560789112949495459501737958331952853208805511
@@ -23,8 +24,8 @@
 # 84580156166097919133875499200524063689912560717606
 # 05886116467109405077541002256983155200055935729725
 # 71636269561882670428252483600823257530420752963450
-
-# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?"
+#
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
 
 # From V1 of this script we know our "largest_num" cant contain any 0's or else the product will be 0
