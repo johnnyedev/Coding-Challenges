933af45cdb00a52ea1a824ac3a820eaec0661418
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-02-20 23:56:33 -0600

    Update and rename 7_10001st_Prime.py to 0007_10001st_Prime.py

--- Python/0007_10001st_Prime.py
+++ Python/0007_10001st_Prime.py
@@ -1,8 +1,9 @@
 # Project Euler
-# Problem 7: https://projecteuler.net/problem=7
-#
-# "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#  What is the 10001st prime number?"
+# Problem 7: 10001st Prime
+# https://projecteuler.net/problem=7
+
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10001st prime number?
 
 
 # Check if number is Prime
