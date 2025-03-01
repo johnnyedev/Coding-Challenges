5eb059871aa79255ed1513f2810a7cbc1874367b
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-02-28 20:22:09 -0600

    Create 0009_Palindrome_Number_v2.py

---
+++ LeetCode/Python/0009_Palindrome_Number_v2.py
@@ -0,0 +1,32 @@
+# LeetCode
+# Problem 9: Palindrome Number
+# https://leetcode.com/problems/palindrome-number
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+# Example 1:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+# Example 2:
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# Example 3:
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+# Constraints:
+# -231 <= x <= 231 - 1
+
+class Solution:
+	def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+
+        if x_str == x_str[::-1]:
+            return True
+        else:
+            return False
